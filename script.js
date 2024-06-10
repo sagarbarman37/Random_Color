613b4abea2352169a1bfe6205ae41d815d1f9752
sagarbarman37 <167900058+sagarbarman37@users.noreply.github.com>
2024-06-10 08:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const generateColor = document.querySelector('#btn');
 const body = document.querySelector('body');
-
+const input = document.querySelector('#input');
 
 
 function color(){
-  return Math.random().toString(16).slice(2, 8).padEnd(6, 0).toUpperCase();
+const randomNum = Math.random().toString(16).slice(2, 8).padEnd(6, 0).toUpperCase();
+
+const hexCode = '#'+ randomNum;
+
+body.style.backgroundColor = hexCode;
+
+input.value = hexCode;
+  
 }
 
+color()
 
 generateColor.addEventListener('click', ()=>{
-  const colorCode = `#${color()}`;
-  body.style.backgroundColor = colorCode;
-  const input = document.querySelector('#input');
-  input.value = colorCode;
-  
-  
+  color()
+
 })
 
