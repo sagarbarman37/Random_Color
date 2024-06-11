aa277618e107059ae8c236b45c37d33d01e1b0a8
sagarbarman37 <167900058+sagarbarman37@users.noreply.github.com>
2024-06-11 21:24:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,18 @@ const input = document.querySelector('#input');
 function color(){
 const randomNum = Math.random().toString(16).slice(2, 8).padEnd(6, 0).toUpperCase();
 
-const hexCode = '#'+ randomNum;
-
+const hexCode = `#${randomNum}`;
 body.style.backgroundColor = hexCode;
-
 input.innerText = hexCode;
-
 btn.style.backgroundColor = hexCode;
-
 btn.style.color = 'white';
-
 navigator.clipboard.writeText(hexCode);
+
+const newBox = document.createElement('div');
+  newBox.classList.add('js-box')
+  body.append(newBox);
+  newBox.style.backgroundColor = hexCode;
+  newBox.innerText = hexCode;
   
 }
 
